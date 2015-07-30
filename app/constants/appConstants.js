ae81cf8b0d046f0164f19c7f4af809926c23d3d9
Sarah McAlear <sarah.mcalear@gmail.com>
2015-07-29 22:08:40 -0700

    changes firebase database

--- app/constants/appConstants.js
+++ app/constants/appConstants.js
@@ -1,6 +1,6 @@
 var appConstants = {
 
-    FIREBASE_HOST: "https://ueberraschungsei.firebaseio.com/",
+    FIREBASE_HOST: "https://lost-in-translation.firebaseio.com/",
     GET_DATA: "GET_DATA",
     ADD_ITEM: "ADD_ITEM",
     REMOVE_ITEM: "REMOVE_ITEM"
